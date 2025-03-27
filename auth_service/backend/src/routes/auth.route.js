d13d4adabf49814d3af94b947081d74151876cd4
Janindu Chamod <janiduchamod2001425@gmail.com>
2025-03-27 22:39:02 +0530

    Update auth.route.js

--- auth_service/backend/src/routes/auth.route.js
+++ auth_service/backend/src/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import {signUpCustomers} from "../controllers/auth.controller.js";
+import {signUpCustomers, signUpOtherRoles} from "../controllers/auth.controller.js";
+import {protectRoute, protectSystemAdminRoutes} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signUpCustomers);
+router.post("/signup/customer", signUpCustomers);
+router.post("/signup/admin", protectRoute, protectSystemAdminRoutes, signUpOtherRoles);
 
 export default router;
