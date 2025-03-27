9fb54daddb1e05767b7ece0747b589693cff284c
Janindu Chamod <janiduchamod2001425@gmail.com>
2025-03-27 15:37:37 +0530

    Update index.js

--- auth_service/backend/src/index.js
+++ auth_service/backend/src/index.js
@@ -6,6 +6,10 @@ import cors from "cors";
 // Database connection
 import {connectDB} from "./lib/db.js";
 
+// Import Routes
+import authRoutes from "./routes/auth.route.js";
+import {errorHandler} from "./middleware/error.middleware.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -20,8 +24,10 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json()); // Middleware to parse JSON
 app.use(express.urlencoded({extended: true})); // Middleware to parse URL-encoded data
+app.use(errorHandler); // Apply global error handler
 
 // Routes
+app.use("/api/v1/auth", authRoutes);
 
 // Start the server on the specified port
 app.listen(PORT, () => {
