a6bde17ac4f9b4424faa370d17b121593e1d07b4
Janindu Chamod <janiduchamod2001425@gmail.com>
2025-04-18 10:09:42 +0530

    Create useAuthStore.js

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,88 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { axiosAuthInstance} from "../lib/axios.js";
+import toast from 'react-hot-toast';
+
+const BASE_URL =
+    import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+
+export const useAuthStore = create(persist(
+    (set, get) => ({
+        authUser: null,
+        isSigningUp: false,
+        isLoggingIn: false,
+        isUpdatingProfile: false,
+        isCheckingAuth: true,
+
+        checkAuth: async () => {
+            try {
+                const res = await axiosAuthInstance.get('/auth/check');
+                set({ authUser: res.data });
+            } catch (error) {
+                console.log('Error in CheckAuth', error);
+                set({ authUser: null });
+            } finally {
+                set({ isCheckingAuth: false });
+            }
+        },
+
+        signup: async (data, isAdmin = false) => {
+            set({ isSigningUp: true});
+            try {
+                const endpoint = isAdmin ? '/admin/signup' : '/auth/signup';
+                const res = await axiosAuthInstance.post(endpoint);
+                set({ authUser: res.data});
+                toast.success('Account created successfully');
+            } catch (error) {
+                toast.error(error.response.data.message);
+            } finally {
+                set({ isSigningUp: false });
+            }
+        },
+
+        login: async (data) => {
+            set({ isLoggingIn: true });
+            try {
+                const res = await axiosAuthInstance.post('/auth/login', data);
+                set({ authUser: res.data });
+                toast.success('Logged in successfully');
+            } catch (error) {
+                toast.error(error.response.data.message);
+            } finally {
+                set({ isLoggingIn: false });
+            }
+        },
+
+        logout: async () => {
+            try {
+                await axiosAuthInstance.post('/auth/logout');
+                set({ authUser: null });
+                toast.success('Logged out successfully');
+            } catch (error) {
+                toast.error(error.response.data.message);
+            }
+        },
+
+        completeProfile: async (data) => {
+            set({ isUpdatingProfile: true });
+            try {
+                const res = await axiosAuthInstance.put('/auth/complete-profile', data);
+                set({ authUser: res.data });
+                toast.success('Profile updated successfully');
+            } catch (error) {
+                toast.error(error.response.data.message);
+            } finally {
+                set({ isUpdatingProfile: false });
+            }
+        },
+
+        getProfile: async () => {
+            try {
+                const res = await axiosAuthInstance.get('/auth/profile');
+                set({ authUser: res.data });
+            } catch (error) {
+                toast.error(error.response.data.message);
+            }
+        }
+    })
+));
