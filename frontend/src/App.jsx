a83e324b7105698379ca46a38b69a34729836443
Janindu Chamod <janiduchamod2001425@gmail.com>
2025-04-20 15:43:54 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,26 @@
 import { useAuthStore } from "./store/useAuthStore.js";
 import { useEffect, useState } from "react";
-import Navbar from "./components/Navbar.jsx";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+
+import Navbar from "./components/common/Navbar.jsx";
+import PublicNavbar from "./components/common/PublicNavbar.jsx";
 
 // Lottie Animation
 import Lottie from "lottie-react";
 import burgerAnimation from "./assets/burger.json";
 
 // Pages
-import HomePage from "./pages/HomePage.jsx";
-import SignUpPage from "./pages/SignUpPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import ProfilePage from "./pages/ProfilePage.jsx";
-import IntroPage from "./pages/IntroPage.jsx";
+import HomePage from "./pages/common/HomePage.jsx";
+import SignUpPage from "./pages/auth/SignUpPage.jsx";
+import LoginPage from "./pages/auth/LoginPage.jsx";
+import ProfilePage from "./pages/auth/ProfilePage.jsx";
+import IntroPage from "./pages/common/IntroPage.jsx";
+import CartPage from "./components/order/CartPage.jsx";
 
 function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
   const [isAppLoading, setIsAppLoading] = useState(true); // Splash screen state
+  const location = useLocation();
 
   // Initial mount effect
   useEffect(() => {
@@ -56,25 +60,38 @@ function App() {
       </div>
     );
 
-  return (
-    <div>
-      <Navbar />
+  const publicPaths = ["/intro", "/signup", "/login"];
+  const isPublicPath = publicPaths.includes(location.pathname) && !authUser;
 
-      <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <IntroPage />} />
-        <Route
-          path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/profile"
-          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-        />
-      </Routes>
+  return (
+    <div className="min-h-screen flex flex-col">
+      {isPublicPath ? <PublicNavbar /> : <Navbar />}
+
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/intro" element={<IntroPage />} />
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to="/intro" />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile"
+            element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/cart"
+            element={authUser ? <CartPage /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </main>
     </div>
   );
 }
