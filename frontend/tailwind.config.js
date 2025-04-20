0611c9284eb49eb3da4865a88ba0c9c1b49c7e38
Janindu Chamod <janiduchamod2001425@gmail.com>
2025-04-20 09:43:38 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}", // Covers all React components
-    ],
-    theme: {
-        extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Covers all React components
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        bree: ["Bree Serif", "serif"],
+        caveat: ["Caveat", "cursive"],
+        comfort: ["Comfort", "cursive"],
+        gloria: ["Gloria Hallelujah", "cursive"],
+        indie: ["Indie Flower", "cursive"],
+        macondo: ["Macondo", "cursive"],
+        protest: ["Protest Riot", "cursive"],
+        shadows: ["Shadows Into Light", "cursive"],
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
