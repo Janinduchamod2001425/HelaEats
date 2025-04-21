1caa0e5f2f8636158edcdb3f9cc11f35ecc74b31
Janindu Chamod <janiduchamod2001425@gmail.com>
2025-04-21 09:11:06 +0530

    Update HomePage.jsx

--- frontend/src/pages/common/HomePage.jsx
+++ frontend/src/pages/common/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="bg-orange-300 pt-[60px]">
+    <div className="bg-orange-300 sm:pt-[60px] mt-28 sm:mt-0">
       <h1>Welcome to the Home Page</h1>
       <p>This is the home page of our application.</p>
       <p>Feel free to explore!</p>
