770a60533d00595b57a803fe63189d70cc408f11
Janindu Chamod <janiduchamod2001425@gmail.com>
2025-04-22 16:05:55 +0530

    Update AdminNavbar.jsx

--- frontend/src/components/common/AdminNavbar.jsx
+++ frontend/src/components/common/AdminNavbar.jsx
@@ -5,7 +5,10 @@ function AdminNavbar() {
     <div className="w-full px-4 md:px-6 py-3 shadow-md bg-black flex flex-wrap items-center justify-between fixed top-0 left-0 z-50">
       {/* Left: Menu + Logo + Toggle */}
       <div className="flex items-center gap-3 sm:gap-4 flex-shrink-0 w-full sm:w-auto mb-3 sm:mb-0">
-        <div className="flex items-center space-x-2">
+        <Link
+          to="/systemadmin/dashboard"
+          className="flex items-center space-x-2"
+        >
           <h1 className="text-lg md:text-xl font-bold tracking-tight">
             <span className="font-bold text-white">Hela</span>{" "}
             <span className="font-extrabold text-yellow-300">
@@ -13,7 +16,7 @@ function AdminNavbar() {
               <span className="text-gray-400 font-caveat">Administration</span>
             </span>
           </h1>
-        </div>
+        </Link>
       </div>
 
       {/* Right: Cart + Profile */}
