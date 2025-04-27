9aa290cc9d67d16c96d924c103b974e61c0f1d92
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2025-04-27 14:38:58 +0530

    Update services in Delivery Service backend
    Just add comments.

--- backend/delivery_service/backend/src/services/driver.service.js
+++ backend/delivery_service/backend/src/services/driver.service.js
@@ -1,23 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
-const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL || 'http://localhost:5001';
+const AUTH_SERVICE_URL =
+  process.env.AUTH_SERVICE_URL || "http://localhost:5001";
 
+// Interface for Get available Drivers Details from Auth Service to Delivery Service
 export const getAvailableDrivers = async () => {
-    try {
-        const response = await axios.get(`${AUTH_SERVICE_URL}/api/drivers/available`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching available drivers:', error.message);
-        throw new Error('Could not fetch available drivers');
-    }
+  try {
+    const response = await axios.get(
+      `${AUTH_SERVICE_URL}/api/drivers/available`,
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching available drivers:", error.message);
+    throw new Error("Could not fetch available drivers");
+  }
 };
 
+// Interface for Update Driver Status in Auth Service After Delivery from Delivery Service
 export const updateDriverStatus = async (driverId, status) => {
-    try {
-        const response = await axios.patch(`${AUTH_SERVICE_URL}/api/drivers/${driverId}/status`, {status});
-        return response.data;
-    } catch (error) {
-        console.error('Error updating driver status:', error.message);
-        throw new Error('Could not update driver status');
-    }
+  try {
+    const response = await axios.patch(
+      `${AUTH_SERVICE_URL}/api/drivers/${driverId}/status`,
+      { status },
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error updating driver status:", error.message);
+    throw new Error("Could not update driver status");
+  }
 };
--- backend/delivery_service/backend/src/services/order.service.js
+++ backend/delivery_service/backend/src/services/order.service.js
@@ -1,13 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
-const ORDER_SERVICE_URL = process.env.ORDER_SERVICE_URL || 'http://localhost:5003';
+const ORDER_SERVICE_URL =
+  process.env.ORDER_SERVICE_URL || "http://localhost:5003";
 
+// Interface for Get Order Details from Order Service to Delivery Service
 export const getOrderDetails = async (orderId) => {
-    try {
-        const response = await axios.get(`${ORDER_SERVICE_URL}/api/orders/${orderId}/delivery-info`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching order details:', error.message);
-        throw new Error('Could not fetch order details');
-    }
-}
+  try {
+    const response = await axios.get(
+      `${ORDER_SERVICE_URL}/api/orders/${orderId}/delivery-info`,
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching order details:", error.message);
+    throw new Error("Could not fetch order details");
+  }
+};
